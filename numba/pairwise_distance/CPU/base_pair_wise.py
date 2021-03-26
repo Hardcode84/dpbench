69096a820f6956c518e2367fa0d4fb3764ab963b
Butygin <ivan.butygin@intel.com>
2021-03-26 18:06:13 +0300

    pairwise_distance: return array instead of passing it as parameter

--- numba/pairwise_distance/CPU/base_pair_wise.py
+++ numba/pairwise_distance/CPU/base_pair_wise.py
@@ -32,12 +32,11 @@ def gen_data(nopt,dims):
     return (
         rnd.random((nopt, dims)),
         rnd.random((nopt, dims)),
-        np.empty((nopt, nopt))
     )
 
 ##############################################
 
-def run(name, alg, sizes=5, step=2, nopt=2**10):
+def run(name, alg, sizes=5, step=2, nopt=2**9):
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--steps', required=False, default=sizes,  help="Number of steps")
@@ -68,13 +67,15 @@ def run(name, alg, sizes=5, step=2, nopt=2**10):
     f2 = open("runtimes.csv",'w',1)
 
     for i in xrange(sizes):
-        X,Y,D = gen_data(nopt,dims)
+        X,Y = gen_data(nopt,dims)
         iterations = xrange(repeat)
 
-        alg(X,Y,D) #warmup
+        D = alg(X,Y) #warmup
+        # if i == 0:
+        #     print(D, flush=True)
         t0 = now()
         for _ in iterations:
-            alg(X,Y,D)
+            alg(X,Y)
 
         mops,time = get_mops(t0, now(), nopt)
         f.write(str(nopt) + "," + str(mops*2*repeat) + "\n")
--- numba/pairwise_distance/CPU/pw_numba.py
+++ numba/pairwise_distance/CPU/pw_numba.py
@@ -8,14 +8,14 @@ import numpy as np
 import numba
 
 @numba.jit(nopython=True,parallel=True,fastmath=True)
-def pw_distance(X1,X2,D):
+def pw_distance(X1,X2):
     #return np.sqrt((np.square(X1 - X2.reshape((X2.shape[0],1,X2.shape[1])))).sum(axis=2))
     x1 = np.sum(np.square(X1), axis=1) #X1=4*3 -> 4*1
     x2 = np.sum(np.square(X2), axis=1) #X2=4*3 -> 4*1
+    x3 = x1.reshape((x1.size,1))
     D = -2 * np.dot(X1, X2.T)
-    x3 = x1.reshape(x1.size,1)
-    D += x3 #x1[:,None] Not supported by Numba
-    D += x2
-    D = np.sqrt(D)
+    D = D + x3 #x1[:,None] Not supported by Numba
+    D = D + x2
+    return np.sqrt(D)
 
 base_pair_wise.run("Numba FastMath", pw_distance)
