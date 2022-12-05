2ff0eea398f2be9ad9446ab75567d4babbbc5eed
Ivan Butygin <ivan.butygin@gmail.com>
2022-12-05 19:53:59 +0100

    kmeans

--- dpbench/benchmarks/kmeans/__init__.py
+++ dpbench/benchmarks/kmeans/__init__.py
@@ -3,9 +3,13 @@
 # SPDX-License-Identifier: Apache 2.0
 
 from .kmeans_initialize import initialize
-from .kmeans_numba_dpex_k import kmeans as kmeans_numba_dpex_k
-from .kmeans_numba_dpex_n import kmeans as kmeans_numba_dpex_n
-from .kmeans_numba_dpex_p import kmeans as kmeans_numba_dpex_p
+
+# from .kmeans_numba_dpex_k import kmeans as kmeans_numba_dpex_k
+# from .kmeans_numba_dpex_n import kmeans as kmeans_numba_dpex_n
+# from .kmeans_numba_dpex_p import kmeans as kmeans_numba_dpex_p
+from .kmeans_numba_dpcomp_k import kmeans as kmeans_numba_dpcomp_k
+from .kmeans_numba_dpcomp_n import kmeans as kmeans_numba_dpcomp_n
+from .kmeans_numba_dpcomp_p import kmeans as kmeans_numba_dpcomp_p
 from .kmeans_numba_n import kmeans as kmeans_numba_n
 from .kmeans_numba_npr import kmeans as kmeans_numba_npr
 from .kmeans_numpy import kmeans as kmeans_numpy
@@ -13,9 +17,12 @@ from .kmeans_python import kmeans as kmeans_python
 
 __all__ = [
     "initialize",
-    "kmeans_numba_dpex_k",
-    "kmeans_numba_dpex_n",
-    "kmeans_numba_dpex_p",
+    # "kmeans_numba_dpex_k",
+    # "kmeans_numba_dpex_n",
+    # "kmeans_numba_dpex_p",
+    "kmeans_numba_dpcomp_k",
+    "kmeans_numba_dpcomp_n",
+    "kmeans_numba_dpcomp_p",
     "kmeans_numba_n",
     "kmeans_numba_npr",
     "kmeans_numpy",
